606887800b6f11e343726f5c586fa747a4da63c4
Koteshwr-rao <koteswararaopakeeri@gmail.com>
2024-07-24 22:10:21 +0530

    ghjk

--- Routes/Admin/adminRoutes.js
+++ Routes/Admin/adminRoutes.js
@@ -33,8 +33,8 @@ router.post('/sendalertmail',async(req,res)=>{
 router.post('/sendsuccessfulsubmissionmail',async(req,res)=>{
   const {name,mail} = req.body
   await sendalertMailtostd(mail,name)
-  .then(()=>console.log("MalPractice Mail sent Successfully"))
-  .catch((err)=>console.log(err))
+  .then(()=>res.send("Submission Mail sent Successfully"))
+  .catch((err)=>res.send(err))
 })
 
 // router.post('/assessments', async (req, res) => {
