46a707a26a1384e6b770fcff9104ef53fa8d7b88
Koteshwr-rao <koteswararaopakeeri@gmail.com>
2024-08-06 00:55:18 +0530

    sdfG: L:

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const sendSuccessfulRegMail = require('./Service/MailService/RegistrationMail')
 const sendLoginDetails = require("./Service/MailService/sendLoginCredentialsMail")
 const app = express()
 require('dotenv').config()
-app.use(cors({
-    origin: 'https://www.epicademy.org',
-  }))
+app.use(cors())
 app.use(express.json());
 app.use('/admin',adminRoutes)
 app.use('/candidates',userRuoutes)
+app.get('/test-cors', (req, res) => {
+    res.json({ message: 'CORS is working' });
+  });
 app.get('/sendmail',async (req,res)=>{
     const assessmentLink = `${process.env.TEST_LINK}/vts-drive2025/VTSAS0001/20KD1A05D1/650c2f2d`
     // await sendSuccessfulRegMail("gantamohan.556@gmail.com",assessmentLink);
@@ -23,7 +24,7 @@ app.get('/sendmail',async (req,res)=>{
 
 
 app.get("/",(req,res)=>{
-    res.send("Hello guru")
+    res.send("Hello There")
 })
 app.listen(5000,()=>{
     console.log("Server is running at port 5000")
